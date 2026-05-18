b0ee7c433156f1daa25dfe82644824a0ca22ee45
Akash-dev5 <AKASH.VERMASKYINFINITE009@GMAIL.COM>
2026-05-19 01:36:35 +0530

    Add pushDockerHub.groovy for Docker image deployment

---
+++ vars/pushDockerHub.groovy
@@ -0,0 +1,19 @@
+def call(String backendImage, String frontendImage) {
+
+    echo "Pushing Docker images"
+
+    withCredentials([usernamePassword(
+        credentialsId: 'dockerhubcred',
+        passwordVariable: 'dockerHubPass',
+        usernameVariable: 'dockerHubUser'
+    )]) {
+
+        sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
+
+        sh "docker image tag djandocicd-backend:latest ${dockerHubUser}/djandocicd-backend:latest"
+        sh "docker push ${dockerHubUser}/djandocicd-backend:latest"
+
+        sh "docker image tag djandocicd-frontend:latest ${dockerHubUser}/djandocicd-frontend:latest"
+        sh "docker push ${dockerHubUser}/djandocicd-frontend:latest"
+    }
+}
