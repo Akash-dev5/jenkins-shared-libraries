961423b698961508797092e7e8e4342f5461a46c
Akash-dev5 <AKASH.VERMASKYINFINITE009@GMAIL.COM>
2026-05-31 19:34:24 +0530

    Update Docker image tags for djangocicd project

--- vars/pushDockerHub.groovy
+++ vars/pushDockerHub.groovy
@@ -10,10 +10,10 @@ def call(String backendImage, String frontendImage) {
 
         sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
 
-        sh "docker image tag djandocicd-backend:latest ${dockerHubUser}/djandocicd-backend:latest"
-        sh "docker push ${dockerHubUser}/djandocicd-backend:latest"
+        sh "docker image tag djangocicd-backend:latest ${dockerHubUser}/djangocicd-backend:latest"
+        sh "docker push ${dockerHubUser}/djangocicd-backend:latest"
 
-        sh "docker image tag djandocicd-frontend:latest ${dockerHubUser}/djandocicd-frontend:latest"
-        sh "docker push ${dockerHubUser}/djandocicd-frontend:latest"
+        sh "docker image tag djangocicd-frontend:latest ${dockerHubUser}/djangocicd-frontend:latest"
+        sh "docker push ${dockerHubUser}/djangocicd-frontend:latest"
     }
 }
