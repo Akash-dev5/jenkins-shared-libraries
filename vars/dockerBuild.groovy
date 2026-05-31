0cff5d5b551e29827fe3c2dcbb800442ce09abd2
Akash-dev5 <AKASH.VERMASKYINFINITE009@GMAIL.COM>
2026-05-31 23:25:29 +0530

    Refactor dockerBuild function to accept tags

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,8 +1,16 @@
-def call() {
+/*def call() {
 
     echo "Building Docker images"
 
     sh "docker compose build"
 
+    echo "Docker build completed"
+} */
+
+def call(String backendTag, String frontendTag) {
+    echo "Building Docker images"
+    sh "docker compose build"
+    sh "docker image tag djangocicd-backend:latest djangocicd-backend:${backendTag}"
+    sh "docker image tag djangocicd-frontend:latest djangocicd-frontend:${frontendTag}"
     echo "Docker build completed"
 }
