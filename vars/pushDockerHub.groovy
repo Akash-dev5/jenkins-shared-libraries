9d12decbf8ffd4b36da8eb087ccbda63964b6070
Akash-dev5 <AKASH.VERMASKYINFINITE009@GMAIL.COM>
2026-05-31 23:24:43 +0530

    Refactor pushDockerHub function to include tags

--- vars/pushDockerHub.groovy
+++ vars/pushDockerHub.groovy
@@ -1,4 +1,4 @@
-def call(String backendImage, String frontendImage) {
+/*def call(String backendImage, String frontendImage) {
 
     echo "Pushing Docker images"
 
@@ -9,11 +9,20 @@ def call(String backendImage, String frontendImage) {
     )]) {
 
         sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
-
-        
         sh "docker push ${dockerHubUser}/djangocicd-backend:latest"
-
-        
         sh "docker push ${dockerHubUser}/djangocicd-frontend:latest"
+    }
+ } */
+
+def call(String backendImage, String frontendImage, String backendTag, String frontendTag) {
+    echo "Pushing Docker images"
+    withCredentials([usernamePassword(
+        credentialsId: 'dockerhub-credentials',
+        passwordVariable: 'dockerHubPass',
+        usernameVariable: 'dockerHubUser'
+    )]) {
+        sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
+        sh "docker push ${dockerHubUser}/${backendImage}:${backendTag}"
+        sh "docker push ${dockerHubUser}/${frontendImage}:${frontendTag}"
     }
 }
