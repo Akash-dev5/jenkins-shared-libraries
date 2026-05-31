915dcf912e867141b78da8837ae9a7e7313a9e5f
Akash-dev5 <AKASH.VERMASKYINFINITE009@GMAIL.COM>
2026-05-31 23:55:59 +0530

    Update deployment function to use version tags
    Refactor deployment function to accept backend and frontend tags as parameters, and update deployment process to pull specific versions.

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,8 +1,26 @@
-def call() {
+/*def call() {
   
 sh "docker compose down"
 
 sh "docker compose up -d"
 
  echo "Application deployed successfully"
+} */
+
+def call(String backendTag, String frontendTag) {
+    echo "Deploying application version Backend:${backendTag} Frontend:${frontendTag}"
+    
+    // stop old containers
+    sh "docker compose down"
+    
+    // pull latest versioned images from dockerhub
+    sh "docker pull acethedata/djangocicd-backend:${backendTag}"
+    sh "docker pull acethedata/djangocicd-frontend:${frontendTag}"
+    
+    // start new containers
+    sh "docker compose up -d"
+    
+    echo "Application deployed successfully!"
+    echo "Backend version: ${backendTag}"
+    echo "Frontend version: ${frontendTag}"
 }
